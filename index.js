2885287556008050b033d45581d8c16282a67bdb
michelleurrea <michelleurrea@gmail.com>
2019-09-13 14:21:32 -0700

    Added routes to the favorites page and a route to each individual favorite

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 require('dotenv').config();
-const express = require('express');
-const axios = require('axios'); 
-const ejsLayouts = require('express-ejs-layouts');
-const app = express();
-const port = process.env.PORT || 3000;
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var axios = require('axios'); 
+var ejsLayouts = require('express-ejs-layouts');
+
+var methodOverride = require('method-override');
+var db = require('./models');
+
+var app = express();
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use('/', express.static('public'));
+
+app.use(methodOverride('_method'))
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
-  axios.get(pokemonUrl).then( function(apiResponse) {
-    var pokemon = apiResponse.data.results;
+  request(pokemonUrl, function(error, response, body) {
+  	var pokemon = JSON.parse(body).results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
-  })
+  });
 });
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
 
-var server = app.listen(port, function() {
-  console.log('...listening on', port );
-});
+var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,50 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+var db = require('../models');
+var request = require('request');
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  	db.pokemon.findAll()
+  	.then(favorites => {
+  		res.render('pokemons/show', { pokemon: favorites })
+  	})
+  	.catch(err => {
+		console.log('error', err)
+		res.send('error')
+	})
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/', (req, res,) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  })
+  .spread(function(poke, wasCreated){
+    res.redirect('pokemon');
+  })
+  .catch((err) => {
+    console.log('ERROR HAPPENED', err)
+    res.render('error')
+  })
 });
+
+router.get('/:name', (req, res) => {
+  var pokeName = req.params.name;
+  var pokeUrl = 'http://pokeapi.co/api/v2/pokemon/' + pokeName
+  console.log(pokeUrl);
+  request(pokeUrl, function(error, response, body){
+    if(error || response.statusCode != 200){
+      res.render('error');
+    }
+    else {
+      var pokeInfo = JSON.parse(body);
+      res.render('pokemons/favorite', { pokeInfo: pokeInfo })
+    }
+  });
+})
 
 module.exports = router;
